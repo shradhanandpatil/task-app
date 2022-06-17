96991bece55750344c4800f0a513fcd7c6f96710
shradhanand <shradhanand@nimapinfotech.com>
2022-06-17 18:46:53 +0530

    get data useing axios

--- src/Axios/index.js
+++ src/Axios/index.js
@@ -3,13 +3,26 @@ import React from 'react'
 
 function Index() {
     const handelClick=()=>{
-        axios.get('http://localhost:8000/user1').then((res)=>{
-            console.log(res.firstName);
+        axios.get('http://localhost:8000/userInfo').then((res)=>{
+            console.log(res.data[0].firstName);
+            console.log(res.data[0].lastName);
         });
     }
+
+    const handelAsync=async()=>{
+        try{
+         const res= await axios.get('http://localhost:8000/userInfo')
+         console.log(res.data[0].firstName);
+         console.log(res.data[0].lastName);
+        }catch(error){
+            console.log(error);
+        }
+    }
+
   return (
     <>
       <button onClick={handelClick}>Click Me</button>
+      <button onClick={handelAsync}>async button</button>
     </>
   )
 }
